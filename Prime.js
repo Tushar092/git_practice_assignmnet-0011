f8e7feea95e86146609627e7add28ad341d31dc3
Akatsuki-123 <simantkubde01@gmail.com>
2022-11-03 17:24:38 +0530

    Fixed Prime Problem

--- Prime.js
+++ Prime.js
@@ -1,10 +1,15 @@
-let bag = ""
-let num = 13
 
-for(let i = 0; i <= num; i++){
-	if(num % 2 == 0){
-		console.log("Prime");
-		}else{
-		console.log("Not Prime");
-		}
-}
+
+let num = 13
+let count=0; 
+
+for(let i = 1; i <= num; i++){
+	if(num % i == 0){
+		count++;
+	}
+  }
+	if(count==2){
+	 console.log("Prime");
+	  }else{
+	    console.log("Not Prime");
+	}
