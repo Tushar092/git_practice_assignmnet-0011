a2baf4afe2bf883bbe49f4ef13347a6b5c572456
Tushar Singh <karansinghrajora1996@gmail.com>
2022-11-03 17:38:34 +0530

    Fixed the Prime code

--- Prime.js
+++ Prime.js
@@ -1,10 +1,22 @@
-let bag = ""
-let num = 13
+function checkPrime(num){
+let counter = 0;
+for(let i = 1; i <= num; i++){
+	if(num % i == 0){
+		counter++;
+	}
+	
+	if(counter == 2){
+		return true;
+	}else{
+		return false;
+	}
+}
+}
 
-for(let i = 0; i <= num; i++){
-	if(num % 2 == 0){
-		console.log("Prime");
-		}else{
-		console.log("Not Prime");
-		}
+let answer = checkPrime(13);
+
+if(answer == true){
+	console.log("It is a Prime Number");
+}else{
+	console.log("It is Not a Prime Number");
 }
